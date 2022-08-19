7ca56873fa1b76c2b13059201c64e6e5e8642603
Semaff <semarc-zhu@yandex.ru>
2022-08-19 16:43:46 +0300

    Created SignIn page

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,6 +1,12 @@
+import AuthForm from "../components/AuthForm";
+
 const Signin = () => {
     return (
-        <div></div>
+        <div className="container">
+            <h1 className="title">Sign In Page</h1>
+            
+            <AuthForm />
+        </div>
     )
 }
 
